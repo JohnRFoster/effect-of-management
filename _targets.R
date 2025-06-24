80d88f1b96d94b4763986030e60e6d5cf782ff24
JohnRFoster <fosterj@bu.edu>
2025-06-24 15:03:44 -0600

    finish growth analysis using targets

--- _targets.R
+++ _targets.R
@@ -10,7 +10,6 @@ library(targets)
 # Set target options:
 tar_option_set(
   packages = c(
-    "tibble",
     "dplyr",
     "tidyr",
     "readr",
@@ -29,15 +28,60 @@ tar_source()
 # Replace the target list below with your own:
 list(
   tar_target(
-    name = model_data_last,
-    command = read_rds(file.path(data_dir, last_iter, "modelData.rds")),
+    name = model_data_last_file,
+    command = file.path(data_dir, last_iter, "modelData.rds"),
     format = "file"
     # format = "qs" # Efficient storage for general data objects.
   ),
 
   tar_target(
-    name = density_last,
-    command = read_rds(file.path(data_dir, last_iter, "densitySummaries.rds")),
+    name = model_data_last,
+    command = get_rds(model_data_last_file)
+  ),
+
+  tar_target(
+    name = density_last_file,
+    command = file.path(data_dir, last_iter, "densitySummaries.rds"),
     format = "file"
+  ),
+
+  tar_target(
+    name = density_last,
+    command = get_rds(density_last_file)
+  ),
+
+  tar_target(
+    name = data_mis,
+    command = data_prep(model_data_last, density_last, cutoff_date)
+  ),
+
+  tar_target(
+    name = realized_growth,
+    command = calculate_realized_r(data_mis)
+  ),
+
+  tar_target(
+    name = mu_lambda,
+    command = mean_growth(realized_growth)
+  ),
+
+  tar_target(
+    name = plot_lambda_hat_by_state,
+    command = plot_mean_lambdas(mu_lambda)
+  ),
+
+  tar_target(
+    name = actual_percent_take,
+    command = prop_take_by_method(model_data_last, mu_lambda, density_last)
+  ),
+
+  tar_target(
+    name = theoretical_percent_take,
+    command = prop_to_remove(realized_growth)
+  ),
+
+  tar_target(
+    name = plot_prop_take,
+    command = plot_percent_take(actual_percent_take, theoretical_percent_take)
   )
 )
