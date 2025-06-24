eabd503f3557f9ac4bce74ee0260b2e0b76ec3a8
JohnRFoster <fosterj@bu.edu>
2025-06-24 15:02:48 -0600

    lubridate

--- R/objects_and_dirs.R
+++ R/objects_and_dirs.R
@@ -9,4 +9,4 @@ data_store <- "../data-store"
 
 # cuttoff date for analysis
 # 2023 is the last full year with complete data
-cutoff_date <- ymd("2023-12-31")
+cutoff_date <- lubridate::ymd("2023-12-31")
