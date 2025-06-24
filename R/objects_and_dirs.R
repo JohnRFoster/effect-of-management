de9bcaa458aca85f436e903b9d7f51d3f96f5d7e
JohnRFoster <fosterj@bu.edu>
2025-06-24 09:41:43 -0600

    start of refactor to targets package

---
+++ R/objects_and_dirs.R
@@ -0,0 +1,12 @@
+# project directories
+plot_dir <- "plots"
+data_dir <- "data"
+last_iter <- "11_posterior"
+first_iter <- "1_posterior"
+
+# shared data across projects live here
+data_store <- "../data-store"
+
+# cuttoff date for analysis
+# 2023 is the last full year with complete data
+cutoff_date <- ymd("2023-12-31")
